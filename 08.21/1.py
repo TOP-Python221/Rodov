32f82f41d2947854d0d59ae8bd82f40a76e6689a
RodovR <rodovroman@yandex.ru>
2022-08-28 19:24:20 +0300

    Добавлены два файла с домашней работой

---
+++ 08.21/1.py
@@ -0,0 +1,78 @@
+
+class Tetra:
+    def __init__(self,  x: int, y: int, edge: int = 10,) -> int:
+        self.edge = edge
+        self.x = x
+        self.y = y
+
+
+    def __sub__(self, other) -> float:
+        if isinstance(other, Tetra):
+            first_point = other.x - self.x
+            second_point = other.y - self.y
+            return round((first_point**2 + second_point**2)**0.5, 2)
+        else:
+            raise TypeError('other object isn`t Tetra')
+
+
+    # ДОДЕЛАТЬ:
+    def surface(self, edge):
+        pass
+
+
+    """Расчитывает объём тетраэдра"""
+    def volume(self) -> float:
+        return str(round((self.edge ** 3 / 12) ** 0.5, 2))
+
+
+    #def ....
+
+
+    def __lt__(self, other):
+        if isinstance(other, Tetra):
+            if tetra1.volume() < tetra2.volume():
+                return  True
+            else:
+                return False
+        else:
+            raise TypeError('other object isn`t Tetra. Look at the code a little bit better and try again :p')
+
+"""Экземпляры класса Tetra"""
+tetra1 = Tetra(3, 4, 12)
+tetra2 = Tetra(3, 4, 15)
+
+
+print(f'Объём первого тэтраэдра: {tetra1.volume()}')
+print(f'Объём второго тэтраэдра: {tetra2.volume()}')
+
+
+# ---------------- Черновые варианты: ----------------
+
+#print(f'Объём первого тетраэдра { = }')
+#print(f'Объём второго тетраэдр {= }')
+
+#class Tetra:
+    #ef __init__(self, edge: int = 10 ) -> float:
+        #self.edge = edge
+
+
+    #def volume(self, tetra1, tetra2):
+        #self.tetra1 = tetra1
+        #self.tetra2 = tetra2
+        #self.Vtetra1 = str(round((self.tetra1 ** 3 / 12) * 1.41, 2))
+        #self.Vtetra2 = str(round((self.tetra2 ** 3 / 12) * 1.41, 2))
+        #return volume
+
+#= str(round((tetra1 ** 3 / 12) * 1.41, 2))
+#tetra1 = Tetra(12)
+#tetra2 = Tetra(15)
+
+#tetra2 = 12
+#etra1 = 15
+
+
+
+
+#print(tetra1.volume())
+#volume1 = round((tetra1 ** 3 / 12) * 1.41, 2)
+#volume2 = round((tetra2 ** 3 / 12) * 1.41, 2)
---
+++ 08.21/2.py
@@ -0,0 +1,75 @@
+import datetime as dt
+
+
+class DateTime:
+    def __init__(self, time_zone: str) -> str:
+        self.time_zone = time_zone
+
+
+    """Информирует пользователя какой часовой пояс он выбрал в зависимости от введённого значения"""
+    def inf(self) -> str:
+        if self.time_zone == '2':
+            print('Kaliningrad Time')
+
+        elif self.time_zone == '3':
+            print('Moscow Time')
+
+        elif self.time_zone == '4':
+            print('Samara Time')
+
+        elif self.time_zone == '5':
+            print('Yekaterinburg Time')
+
+        elif self.time_zone == '6':
+            print('Omsk Time')
+
+        elif self.time_zone == '7':
+            print('Krasnoyarsk Time')
+
+        elif self.time_zone == '8':
+            print('Irkutsk Time')
+
+        elif self.time_zone == '9':
+            print('Yakutsk Time')
+
+        elif self.time_zone == '10':
+            print('Vladivostok Time')
+
+        elif self.time_zone == '11':
+            print('Magadan Time')
+
+        elif self.time_zone == '12':
+            print('Kamchatka Time')
+
+        else:
+            print('Сказано же: от 2 до 12!')
+
+    """Указывает количество часов в другом указанном пользователе регионе. К сожалению, смог додумать только то, 
+    как можно вывести количество часов, а не полное время."""
+    def diff(self) -> str:
+        date_t = str(dt.datetime.today()).split(' ')[1]
+        hour = date_t.split(':')[0]
+        print('Время в чужом часовом поясе: ')
+        print(int(time_zone2.__dict__['time_zone']) + int(hour))
+
+"""Экземпляры класса DateTime"""
+time_zone1 = DateTime(input('Введите Ваш часовой пояс России в UTC(от 2 до 12): '))
+time_zone2 = DateTime(input('Введите чужой часовой пояс России В UTC(от 2 до 12): '))
+
+
+print(time_zone1.inf())
+print(time_zone1.diff())
+
+print(time_zone2.inf())
+print(time_zone1.diff())
+
+
+
+
+
+#date_t = str(dt.datetime.today()).split(' ')[1]
+#hour = date_t.split(':')[0]
+#print(date_t)
+#print(f'Часы: {hour}', f'Тип: {type(hour)}')
+#print(type(date_t))
+#print(time_zone1)
