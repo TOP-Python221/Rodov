91e36934cfa5040521683ba8388b549626d7d8f7
Gennadiy <renardes@gmail.com>
2022-10-15 00:39:15 +0500

    06.12: комментарии, исправления, итоги

--- 06.12/1.95.py
+++ 06.12/1.95.py
@@ -1,15 +1,51 @@
+from string import punctuation, whitespace
+
+punctuation += '’'
+stops = ('.', '!', '?')
+
+
 # ДОБАВИТЬ: аннотации типов параметров и возвращаемого значения
-def newmessage(message):
+def sentence_case(message):
     # ДОБАВИТЬ: документацию функции
-    # print(type(message))
+
+    message = message.strip().capitalize() + ' '
+    # КОММЕНТАРИЙ: по-хорошему, здесь, конечно, нужна регулярка — но это не значит, что нельзя справиться без неё: вам нужно рассмотреть все случаи, когда I является самостоятельным словом, но при этом может быть окружено символами пространства и знаками препинания
+    # ИСПОЛЬЗОВАТЬ: если бы мне было пофиг на производительность — а в этой задаче с короткими фразами на неё явно можно забить — то я бы написал что-то вроде такого перебора:
+    for start_char in punctuation + whitespace:
+        for end_char in punctuation + whitespace:
+            substr = f'{start_char}i{end_char}'
+            replace = f'{start_char}I{end_char}'
+            message = message.replace(substr, replace)
+
     words = message.split(' ')
-    for i in range(len(words)):
-        if '?' in words[i]:
-            upper = words[i+1].capitalize()
-            words[i+1] = upper
-            # print(words[i+1].capitalize())
-            upper = ' '.join(words)
-    print(upper)
+    for i, word in enumerate(words):
+        if word.endswith(stops):
+            words[i+1] = words[i+1].capitalize()
+    clear_text = ' '.join(words)
+    return clear_text.strip()
+
+
+# tests:
+print(sentence_case('what time do i have to be there? what’s the address? this time i’ll try to be on time!'))
+# What time do I have to be there? What’s the address? This time I’ll try to be on time!
+
+
+# КОММЕНТАРИЙ: местами вы довольно близко подобрались =) но в разных фрагментах
+# СДЕЛАТЬ: изучите представленный код функции
+
+
+# ИТОГ: половину зачитываю за старание — 3/6
+
+
+    # # print(type(message))
+    # words = message.split(' ')
+    # for i in range(len(words)):
+    #     if '?' in words[i]:
+    #         upper = words[i+1].capitalize()
+    #         words[i+1] = upper
+    #         # print(words[i+1].capitalize())
+    #         upper = ' '.join(words)
+    # print(upper)
             # if words[0].startswith('w'):
                 # print(words[0])
                 # words[0].capitalize() 
@@ -40,7 +76,3 @@ def newmessage(message):
     # print(words)
     
 # Ну, и это третья попытка возвести в верхний регистр букву i  
-    
-newmessage('what time do i have to be there? what’s the address? this time i’ll try to be on time!')
-
- 
